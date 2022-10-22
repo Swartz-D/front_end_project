ae543b0063b4b9dbf3e4c668e235f85c941d64d0
Dustin Swartzbaugh <dustin.swartzbaugh@gmail.com>
2022-10-22 00:35:13 -0400

    copied old code

---
+++ app.js
@@ -0,0 +1,37 @@
+/* eslint-env jquery */
+
+const body = document.querySelector('body');
+let newDiv = document.createElement('div');
+
+$('#submit').on('click', function(){
+  event.preventDefault();
+  var userInput = $('#bookGenre').val().toLowerCase();
+  console.log(userInput);
+  var formatted = userInput.replace(/ /g, '_');
+  console.log(formatted);
+  $.get('https://openlibrary.org/subjects/'+formatted+'.json', (data) => {
+    console.log(data);
+    let bookList = document.createElement('div');
+    bookList.classList.add('container');
+    for(let i=0; i<data.works.length; i++){
+      let newBook = document.createElement('div');
+      newBook.classList.add('book-info');
+      let currentImg = $(`<img src="https://covers.openlibrary.org/b/id/${data.works[i].cover_id}-L.jpg" class="img-fluid" />`);
+      console.log(currentImg);
+      newBook.innerHTML = 'Book title: '+ data.works[i].title+' | '+ 'Author(s): '+data.works[i].authors[0].name;
+      bookList.append(newBook);
+      bookList.append(currentImg[0]);      
+    
+    }
+    console.log(newDiv.children[0]);
+    if(newDiv.children[0]===undefined){
+      newDiv.appendChild(bookList);
+      body.appendChild(newDiv);
+      console.log('newDiv', newDiv.children[0]);
+    }else{
+      newDiv.innerHTML = '';
+      newDiv.appendChild(bookList);
+      body.appendChild(newDiv); 
+      console.log('newDiv 2', newDiv);
+    }  
+  });});
